260218b01058b7bd045a4dbcbee41b9ace1f6c77
ghostfinger <flukuwi.fl7@gmail.com>
2025-05-22 18:28:44 +0300

    complete front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+// This is the main entry point for the React application.
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+        
+          
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { Button, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header/index.js';
+
+function userForm() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <Header />
+      <h1 className="text-center mt-5">Patient Form</h1>
+      <p className="text-center">Please Doctor fill out the form below:</p>
+
+    <div class="container-fluid mt-5 justify-content-center w-50">
+
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Patient_Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter Patient Name" />
+
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+    <Form.Label>Doctor_Name</Form.Label>
+    <Form.Control type="text" placeholder="Enter Doctor Name" />
+  </Form.Group>
+  
+  <Button variant="primary" type="submit" className="mt-3 w-100">
+    Submit patient Data
+  </Button>
+</Form>
     </div>
+    </div>
+  
   );
 }
 
-export default App;
+export default userForm;
